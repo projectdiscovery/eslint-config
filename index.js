b70241f0f22aea77ee5d59b3304efe9e770b979b
Jesse Kelly <kellyjesse881@gmail.com>
2022-10-28 17:20:39 +0200

    feat: added functional style

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 module.exports = {
-        env: { es2021: true },
+        parser: "@typescript-eslint/parser",
+        env: { es6: true },
         plugins: [
                 "fp-ts",
-                "jsdoc"
+                "jsdoc",
+                "@typescript-eslint"
         ],
         extends: [
                 "airbnb",
                 "airbnb-typescript",
                 "plugin:fp-ts/recommended",
                 "adjunct",
-                "plugin:jsdoc/recommended"
+                "plugin:jsdoc/recommended",
+                "plugin:functional/recommended",
+                "plugin:functional/stylistic"
         ],
         parserOptions: {
                 ecmaFeatures: {
